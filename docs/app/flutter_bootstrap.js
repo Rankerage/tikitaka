a55e139d97be204274b660a316eb58f99e6e5d92
mathbible <mathbible@gmail.com>
2026-08-01 12:58:59 +0900

    🃏 TikiTaka web app: fixed main.dart branding + base href

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1778432592" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2185696827" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
